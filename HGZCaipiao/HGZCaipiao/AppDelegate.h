bdbbf8e66d65296842063c1d3d8ce8a4e359e448
HGZ <18051739494@163.com>
2016-09-26 16:05:16 +0800

    初始化项目

---
+++ HGZCaipiao/HGZCaipiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGZCaiPiao
+//
+//  Created by 韩桂铸 on 16/9/26.
+//  Copyright © 2016年 韩桂铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
